f0133a8102bb49b11bfc762cd40e41f88a35a560
lzw1231 <luozhw.bit@gmail.com>
2026-08-05 23:16:59 +0800

    客户端初步实现功能，与服务器端互动正常

--- ws_3/src/actions_py/actions_py/move_robot_client.py
+++ ws_3/src/actions_py/actions_py/move_robot_client.py
@@ -1,1 +1,61 @@
 #!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionClient
+from rclpy.action.client import ClientGoalHandle, GoalStatus
+from my_robot_interfaces.action import MoveRobot
+
+
+class MoveRobotClientNode(Node):
+    def __init__(self):
+        super().__init__('move_robot_client_node')
+        self.move_robot_client_ = ActionClient(self, MoveRobot, "move_robot")
+
+    def send_goal(self, position, velocity):
+        self.move_robot_client_.wait_for_server()
+
+        goal = MoveRobot.Goal()
+        goal.position = position
+        goal.velocity = velocity
+
+        self.get_logger().info("Send goal with position " + str(position) + " and velocity " + str(velocity))
+        self.move_robot_client_.send_goal_async(goal, feedback_callback=self.goal_feedback_callback).add_done_callback(
+            self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_hand: ClientGoalHandle = future.result()
+        if goal_hand.accepted:
+            self.get_logger().info("Goal got accepted")
+            goal_hand.get_result_async().add_done_callback(self.goal_result_callback)
+        else:
+            self.get_logger().info("Goal got rejected")
+
+    def goal_result_callback(self, future):
+        status = future.result().status
+        result = future.result().result
+
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info("Succeed")
+        elif status == GoalStatus.STATUS_CANCELED:
+            self.get_logger().warn("Canceled")
+        elif status == GoalStatus.STATUS_ABORTED:
+            self.get_logger().error("Aborted")
+
+        self.get_logger().info("Position: " + str(result.position))
+        self.get_logger().info("Message: " + str(result.message))
+
+    def goal_feedback_callback(self, feedback_msg):
+        position = feedback_msg.feedback.current_position
+        self.get_logger().info("Feedback position: " + str(position))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MoveRobotClientNode()
+    node.send_goal(77, 5)
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
