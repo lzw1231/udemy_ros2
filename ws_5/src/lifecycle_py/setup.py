a878e25a8cd33c834c92d0a0a0dca9351d1a601d
lzw1231 <luozhw.bit@gmail.com>
2026-08-09 16:43:59 +0800

    666

--- ws_5/src/lifecycle_py/setup.py
+++ ws_5/src/lifecycle_py/setup.py
@@ -22,6 +22,9 @@ setup(
         ],
     },
     entry_points={
-        "console_scripts": ["number_publisher = lifecycle_py.number_publisher:main"],
+        "console_scripts": [
+            "number_publisher = lifecycle_py.number_publisher:main",
+            "lifecycle_node_manager = lifecycle_py.lifecycle_node_manager:main",
+        ],
     },
 )
