0230d7831fec2a61553d061591ac3ed2f2588e36
lzw1231 <luozhw.bit@gmail.com>
2026-08-12 17:22:10 +0800

    开始第7节，执行器

---
+++ ws_7/src/executors_py/executors_py/number_publisher.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import Int64
+
+
+class NumberPublisherNode(Node):
+    def __init__(self):
+        super().__init__("number_publisher")
+        self.number_ = 1
+        self.publish_frequency_ = 1.0
+        self.number_publisher_ = self.create_publisher(Int64, "number", 10)
+        self.number_timer = self.create_timer(1.0 / self.publish_frequency_, self.publish_number)
+        self.get_logger().info("Number publisher has been started.")
+
+    def publish_number(self):
+        msg = Int64
+        msg.data = self.number_
+        self.number_ += 1
+
+
+def main(args=None):
+    rclpy.init(args=None)
+    node = NumberPublisherNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
